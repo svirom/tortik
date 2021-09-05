fbfa70752d3c0d62c52805076a24f80d1b468d91
svirom <svirom@yahoo.com>
2021-09-05 21:23:23 +0300

    header start

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
 
+  // display submenu on hover
+  $(window).on('load resize', function() {
+    var width = $(document).width();
+
+    if (width >= 768) {
+      // $('.dropdown-hover').hover(
+      //   function() {
+      //     const $this = $(this);
+      //     $(this).addClass('show');
+      //     $(this).find('.dropdown-toggle').attr('aria-expanded', 'true');
+      //     $(this).find('.dropdown-menu').addClass('show');
+      //   },
+      //   function() {
+      //     $(this).removeClass('show');
+      //     $(this).find('.dropdown-toggle').attr('aria-expanded', 'false');
+      //     $(this).find('.dropdown-menu').removeClass('show');
+      //   }
+      // );
+    } else {
+      $(".dropdown").off('mouseenter mouseleave');
+    }
+  });
 
 })
