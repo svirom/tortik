1f1b26a3c7789f9c880c8d5f4d85dacc65ba67d6
svirom <svirom@yahoo.com>
2021-09-21 21:11:29 +0300

    category page

--- js/main.js
+++ js/main.js
@@ -40,4 +40,30 @@ $(document).ready(function() {
       .find('.mobile-catalog').removeClass('active');
   }
 
+  // slider
+  $('#content-slider').lightSlider({
+    item: 6,
+    loop: true,
+    slideMove: 1,
+    easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+    speed: 600,
+    responsive : [
+      {
+        breakpoint: 800,
+        settings: {
+          item: 3,
+          slideMove: 1,
+          slideMargin: 6,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          item: 2,
+          slideMove: 1
+        }
+      }
+    ]
+  });  
+
 })
