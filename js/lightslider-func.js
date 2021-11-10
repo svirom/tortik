aba5839998ab91110e7ab0228ac717a73489d30a
svirom <svirom@yahoo.com>
2021-11-10 18:05:53 +0200

    page invoice

--- js/lightslider-func.js
+++ js/lightslider-func.js
@@ -30,4 +30,38 @@ $(document).ready(function() {
     ]
   });
 
+  // invoice slider
+  var invoiceSlider = $('.invoice-slider').lightSlider({
+    item: 1,
+    loop: true,
+    slideMove: 1,
+    slideMargin: 30,
+    easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+    speed: 600,
+		pager: false,
+    
+    onSliderLoad: function(el) {  
+		  var slideCount = $(el).find('.lslide').length;
+
+      $('#items-amount').text(slideCount);
+      $('#current-item-number').text('1');
+
+      $(el).find('.slider-card').on('click', function() {
+        var currImage = $(this).find('img').attr('src');
+
+        $('#invoice-modal').find('.modal-body img').attr('src', currImage);
+      })
+
+      $('#invoice-modal').on('hidden.bs.modal', function() {
+        $(this).find('.modal-body img').attr('src', '');
+      })
+    },
+
+    onBeforeSlide: function() {
+      var currSlide = invoiceSlider.getCurrentSlideCount();
+
+      $('#current-item-number').text(currSlide);
+    },
+  });
+
 })
